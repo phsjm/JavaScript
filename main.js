f865b0dbb7d34a6b9d7aa84b960e12b628e08ea6
phsjm <pchann.0624@gmail.com>
2026-04-02 06:45:35 +0900

    load eventを使おう

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
 'use strict';
-
-const e = document.getElementById('button');
-e.addEventListener('click', () => {
-    const li = document.createElement('li');
-    const text = document.createTextNode('test');
-    li.appendChild(text);
-    const list = document.getElementById('list');
-    list.appendChild(li);
-}, false);
+window.onload = () => {
+    console.log('load event!');
+}
