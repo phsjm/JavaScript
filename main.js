2431f6673121c1f44a5dc9bec79416a621694b97
phsjm <pchann.0624@gmail.com>
2026-04-04 23:02:42 +0900

    ノードの削除

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 'use strict';
 const eventButton = document.getElementById('button');
 eventButton.addEventListener('click', () => {
-    const newList = document.createElement('li');
-    newList.setAttribute('id', 'newList');
-    const newText = document.createTextNode('new element');
-    newList.appendChild(newText);
-    const oldList = document.getElementById('oldList');
-    const parentNode = oldList.parentNode;
-    parentNode.replaceChild(newList, oldList);
+    const parentElement = document.getElementById('List');
+    const elements = parentElement.getElementsByTagName('li');
+    const removeIndex = elements.length - 1;
+    console.log(elements.length)
+    parentElement.removeChild(elements[removeIndex]);
 }, false);
